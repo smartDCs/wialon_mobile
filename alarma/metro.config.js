6c58add74f4a75eb83e751e5cae3507aa3caf4f9
smartDCs <39224705+smartDCs@users.noreply.github.com>
2025-05-29 08:27:56 -0500

    Delete alarma directory

--- alarma/metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.sourceExts.push('cjs');
-// This is the new line you should add in, after the previous lines
-defaultConfig.resolver.unstable_enablePackageExports = false;
-module.exports = defaultConfig;
--- alarma/src/auth/firebase_config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCb0JB3GOk_wTzp1wsSK3i8lWRBZqkCQjQ",
-  authDomain: "telegrafiaec.firebaseapp.com",
-  databaseURL: "https://telegrafiaec-default-rtdb.firebaseio.com",
-  projectId: "telegrafiaec",
-  storageBucket: "telegrafiaec.firebasestorage.app",
-  messagingSenderId: "811933313348",
-  appId: "1:811933313348:web:676d08e40a1b9a67680f11"
-  };
